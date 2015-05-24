167a3aaec817a22b917dc9625e1410e84289d76a
Alex Vargas <vargas.84@osu.edu>
2015-05-24 16:25:53 -0400

    Hook up Mailchimp.

---
+++ app/controllers/subscribe_controller.rb
@@ -0,0 +1,14 @@
+class SubscribeController < ApplicationController
+  def create
+    email = params[:email]
+
+    if EmailValidator.valid? email
+      mailchimp = Mailchimp::API.new ENV['mailchimp_api_key']
+      mailchimp.lists.subscribe ENV['mailchimp_list_id'], { "email" => email }
+
+      render json: { message: 'success' }.to_json, status: :ok
+    else
+      render json: { message: 'invalid email' }.to_json, status: :bad_request
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,3 @@
 Rails.application.routes.draw do
+  post '/subscribe', to: 'subscribe#create', as: 'subscribe'
 end
