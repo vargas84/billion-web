ee6309949b0861beb4082cbd1026743e3e5048f3
Alex Vargas <vargas.84@osu.edu>
2015-05-25 19:52:42 -0400

    Remove sudo from asset precompile command.

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -3,7 +3,7 @@ rails_env = new_resource.environment['RAILS_ENV']
 Chef::Log.info('Precompiling assets...')
 execute 'precompile assets' do
   cwd release_path
-  command 'sudo RAILS_ENV=production bin/rake assets:precompile'
+  command 'RAILS_ENV=production bin/rake assets:precompile'
   user 'deploy'
   group 'www-data'
   only_if { rails_env == 'production' }
