1329811ffd27b54ae5d117214b039af17e287554
Justin Young <jyoung@citybee.com>
2015-07-15 21:34:26 -0400

    Make rubocop happy

--- app/services/payment.rb
+++ app/services/payment.rb
@@ -1,6 +1,18 @@
 class Payment
-
   class PaymentServiceError < StandardError; end
+
+  BRAINTREE_ERRORS = [Braintree::AuthenticationError,
+                      Braintree::AuthorizationError,
+                      Braintree::ConfigurationError,
+                      Braintree::DownForMaintenanceError,
+                      Braintree::ForgedQueryString,
+                      Braintree::InvalidChallenge,
+                      Braintree::InvalidSignature,
+                      Braintree::NotFoundError,
+                      Braintree::ServerError,
+                      Braintree::SSLCertificateError,
+                      Braintree::UnexpectedError,
+                      Braintree::UpgradeRequiredError]
 
   attr_reader :errors
 
@@ -10,38 +22,18 @@ class Payment
   end
 
   def pay
-      begin
-        result = Braintree::Transaction.sale(
-          :payment_method_nonce => @token,
-          :amount => @amount
-        )
+    result = Braintree::Transaction.sale(payment_method_nonce: @token, amount: @amount)
 
-        if result.success?
-          true
-        else
-          # validation error, e.g. rejected etc
-          @errors = result.errors.map { |error| error.message }
-
-          puts @errors.inspect
-          false
-        end
-      rescue Braintree::AuthenticationError,
-        Braintree::AuthorizationError,
-        Braintree::ConfigurationError,
-        Braintree::DownForMaintenanceError,
-        Braintree::ForgedQueryString,
-        Braintree::InvalidChallenge,
-        Braintree::InvalidSignature,
-        Braintree::NotFoundError,
-        Braintree::ServerError,
-        Braintree::SSLCertificateError,
-        Braintree::UnexpectedError,
-        Braintree::UpgradeRequiredError => e
-
-        # likely a braintree service failure
-        # TODO log to new relic
-        raise PaymentServiceError.new(e.message)
-      end
+    if result.success?
+      true
+    else
+      # validation error, e.g. rejected etc
+      @errors = result.errors.map(&:message)
+      false
+    end
+  rescue *BRAINTREE_ERRORS => e
+    # likely a braintree service failure
+    # TODO: log to new relic
+    raise PaymentServiceError, e.message
   end
-
 end
