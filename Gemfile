1cd94bc28b10612c2c55b9934353c7efd894a7e4
Ishmeet Grewal <ishmeetsinghgrewal@gmail.com>
2015-07-18 22:39:47 -0400

    Added Faker gem, pundit gem and policies, Role model, Role model rspec, updated factories

--- Gemfile
+++ Gemfile
@@ -12,7 +12,8 @@ gem 'lodash-rails', '~> 3.7.0'
 gem 'devise', '~> 3.5.1'
 gem 'devise_invitable', '~> 1.5.1'
 gem 'braintree', '~> 2.45.0'
-gem 'pundit'
+gem 'pundit', '~> 1.0.1'
+gem 'faker', '~> 1.4.3'
 
 # Rails-specific gems
 gem 'rails', '4.2.0'
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe User, type: 'model' do
-  #pending "add some examples to (or delete) #{__FILE__}"
+describe User, type: :model do
+  subject { build :user }
+
+  describe 'associations' do
+    it { is_expected.to belong_to(:role).inverse_of(:users) }
+  end
 end
