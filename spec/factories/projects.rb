277cc3364bd8dcf2cf38a3a9467557791c75ed80
Dicko Sow <s12.dsow@gmail.com>
2015-07-18 22:37:01 -0400

    made rubocop happy with project factory

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :project do
-    name "MyString"
-    blurb "MyText"
-    description "MyText"
+    name 'MyString'
+    blurb 'MyText'
+    description 'MyText'
   end
 end
