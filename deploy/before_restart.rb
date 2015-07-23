328c26802dde128c3310f357785b77e615cef53b
Alex Vargas <vargas.84@osu.edu>
2015-07-23 16:31:53 -0400

    Update production deploy script to include secret key base as environment variable.

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,10 +1,15 @@
 rails_env = new_resource.environment['RAILS_ENV']
+secret_key_base = new_resource.environment['SECRET_KEY_BASE']
 
 Chef::Log.info('Precompiling assets...')
 execute 'precompile assets' do
   cwd release_path
-  command 'RAILS_ENV=production bin/rake assets:precompile'
+  command 'bin/rake assets:precompile'
   user 'deploy'
   group 'www-data'
   only_if { rails_env == 'production' }
+  environment(
+    'RAILS_ENV' => rails_env,
+    'SECRET_KEY_BASE' => secret_key_base
+  )
 end
