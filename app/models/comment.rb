24321f76148be38cf9eab1980a295701d09a9835
Dicko Sow <s12.dsow@gmail.com>
2015-07-26 21:32:02 -0400

    Add validations for comment and competition models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ActiveRecord::Base
+  validates :author, :content, :project, presence: true
+
   belongs_to :project, inverse_of: :comments
 end
--- app/models/competition.rb
+++ app/models/competition.rb
@@ -1,3 +1,5 @@
 class Competition < ActiveRecord::Base
+  validates :code_name, :start_date, :end_date, presence: true
+
   has_many :projects, inverse_of: :competition
 end
