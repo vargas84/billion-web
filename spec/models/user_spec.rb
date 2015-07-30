5486806e7b277d02d8d2a739bb7b56ee0578adaa
Matias Grioni <matgrioni@gmail.com>
2015-07-29 21:41:02 -0400

    Created specs to for admin?

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,15 @@ describe User, type: :model do
   describe 'associations' do
     it { is_expected.to belong_to(:role).inverse_of(:users) }
   end
+
+  describe '#admin?' do
+    let(:admin) { build :admin }
+    let(:user) { build :user }
+    it 'is true for admin' do
+      expect(admin).to be_admin
+    end
+    it 'is false for user' do
+      expect(user).not_to be_admin
+    end
+  end
 end
