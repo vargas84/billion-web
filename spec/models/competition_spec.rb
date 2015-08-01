1c9cc78242a257eff46f7b0fc89dbe45860a5744
s12dsow <s12.dsow@gmail.com>
2015-08-01 16:59:56 -0400

    Competition spec to test validations and associations

---
+++ spec/models/competition_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Competition, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of(:code_name) }
+    it { should validate_presence_of(:start_date) }
+    it { should validate_presence_of(:end_date) }
+  end
+  describe 'associations' do
+    it { is_expected.to have_many(:projects).inverse_of(:competition) }
+  end
+end
