1f804f95fa1a225c9ca169d331e03b2f9fa7d1ce
s12dsow <s12.dsow@gmail.com>
2015-08-01 15:44:22 -0400

    Can optionally create comments and collaborators when create project

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -1,7 +1,19 @@
 FactoryGirl.define do
   factory :project do
-    name 'MyString'
-    blurb 'MyText'
-    description 'MyText'
+    name { Faker::Company.name }
+    blurb { Faker::Company.catch_phrase }
+    description { Faker::Lorem.paragraph }
+
+    trait :with_comments do
+      after :create do |project|
+        create_list(:comment, 2, project: project)
+      end
+    end
+
+    trait :with_collaborators do
+      after :create do |project|
+        create_list(:collaborator, 1, project: project)
+      end
+    end
   end
 end
