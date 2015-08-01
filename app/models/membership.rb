d3f47e8007d9b27f856a43d35eb07925949c5269
s12dsow <s12.dsow@gmail.com>
2015-08-01 15:50:42 -0400

    Validations on membership join table

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,4 +1,6 @@
 class Membership < ActiveRecord::Base
   belongs_to :project, inverse_of: :memberships
   belongs_to :user, inverse_of: :memberships
+
+  validates :project, :user, presence: true
 end
