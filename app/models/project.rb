1ade46c1a21b5855fc6351a74e23440e31e2329a
s12dsow <s12.dsow@gmail.com>
2015-08-01 16:14:54 -0400

    Moved validations below associations in project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,9 +1,9 @@
 class Project < ActiveRecord::Base
-  validates :name, presence: true
-
   belongs_to :competition, inverse_of: :projects
 
   has_many :memberships, inverse_of: :project
   has_many :collaborators, through: :memberships, source: :user, inverse_of: :projects
   has_many :comments, inverse_of: :project, dependent: :destroy
+
+  validates :name, presence: true
 end
