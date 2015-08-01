b3717278dfd31660613a70f9e3e260a3108b1a8a
s12dsow <s12.dsow@gmail.com>
2015-08-01 16:53:15 -0400

    Create specs for project validations and associations

---
+++ spec/models/project_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Project, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+  end
+  describe 'associations' do
+    it { is_expected.to belong_to(:competition).inverse_of(:projects) }
+    it { is_expected.to have_many(:memberships).inverse_of(:project) }
+    it { is_expected.to have_many(:collaborators).inverse_of(:projects) }
+    it { is_expected.to have_many(:comments).inverse_of(:project) }
+  end
+end
