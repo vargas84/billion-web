a61fdc44ed30cf32f1ed410b236c1d466835218a
s12dsow <s12.dsow@gmail.com>
2015-08-01 17:24:32 -0400

    Comment spec with validations and associations

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of(:author) }
+    it { should validate_presence_of(:content) }
+    it { should validate_presence_of(:project) }
+  end
+  describe 'associations' do
+    it { is_expected.to belong_to(:project).inverse_of(:comments) }
+  end
+end
