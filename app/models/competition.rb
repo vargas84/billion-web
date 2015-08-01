aaa0a942d054a0f1368f5097584388cb2aae2fd2
s12dsow <s12.dsow@gmail.com>
2015-08-01 15:38:56 -0400

    Competition model has dependent destroy on projects association, changed factory to use faker and optionally create projects with competition

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -1,5 +1,5 @@
 class Competition < ActiveRecord::Base
   validates :code_name, :start_date, :end_date, presence: true
 
-  has_many :projects, inverse_of: :competition
+  has_many :projects, inverse_of: :competition, dependent: :destroy
 end
--- spec/factories/competitions.rb
+++ spec/factories/competitions.rb
@@ -1,7 +1,13 @@
 FactoryGirl.define do
   factory :competition do
-    code_name 'Code name'
-    start_date '2015-09-19'
-    end_date '2015-09-20'
+    code_name { Faker::Lorem.word }
+    start_date { Faker::Date.between(10.days.from_now, 20.days.from_now) }
+    end_date { Faker::Date.between(25.days.from_now, 30.days.from_now) }
+
+    trait :with_projects do
+      after :create do |competition|
+        create_list(:project, 1, competition: competition)
+      end
+    end
   end
 end
