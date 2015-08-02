aa21fa8095a879913731387dfdaf0e92e91a1675
Ishmeet Grewal <ishmeetsinghgrewal@gmail.com>
2015-08-01 20:15:59 -0400

    User Model Rubocop fixes

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :user do |f|
-    f.first_name Faker::Name.first_name
-    f.last_name Faker::Name.last_name
-    f.email Faker::Internet.email
-    f.password Faker::Internet.password(8)
+    f.first_name { Faker::Name.first_name }
+    f.last_name { Faker::Name.last_name }
+    f.email { Faker::Internet.email }
+    f.password { Faker::Internet.password(8) }
   end
 end
