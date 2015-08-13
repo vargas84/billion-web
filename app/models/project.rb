8a56b90cffe139cd33bf2868f9fa6b4b2604a1e4
s12dsow <s12.dsow@gmail.com>
2015-08-13 17:10:11 -0400

    Added associations to account for polymorphic associations for sent and received transactions

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,6 +4,8 @@ class Project < ActiveRecord::Base
   has_many :memberships, inverse_of: :project
   has_many :collaborators, through: :memberships, source: :user, inverse_of: :projects
   has_many :comments, inverse_of: :project, dependent: :destroy
+  has_many :sent_transactions, as: :sender, class_name: 'Transaction'
+  has_many :received_transactions, as: :recipient, class_name: 'Transaction'
 
   validates :name, presence: true
 end
