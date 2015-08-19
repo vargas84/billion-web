42c48e7b83b88f2116a7f0e0540b5c89637f4354
s12dsow <s12.dsow@gmail.com>
2015-08-19 19:07:49 -0400

    Whitelist sender_type and recipient_type as projects or users

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,5 +2,9 @@ class Transaction < ActiveRecord::Base
   belongs_to :sender, polymorphic: true
   belongs_to :recipient, polymorphic: true
 
+  TYPES = %w(Project User)
+
+  validates :sender_type, inclusion: { in: TYPES }
+  validates :recipient_type, inclusion: { in: TYPES }
   validates :amount, presence: true, numericality: { greater_than: 0 }
 end
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe Transaction, type: :model do
   describe 'validations' do
     it { is_expected.to validate_presence_of(:amount) }
     it { is_expected.to validate_numericality_of(:amount).is_greater_than(0) }
+    it { is_expected.to validate_inclusion_of(:sender_type).in_array(%w(Project User)) }
+    it { is_expected.to validate_inclusion_of(:recipient_type).in_array(%w(Project User)) }
   end
 
   describe 'associations' do
