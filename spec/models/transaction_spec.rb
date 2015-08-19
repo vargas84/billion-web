10aa78a74917c51677a2620e96a5e2164a0f133a
s12dsow <s12.dsow@gmail.com>
2015-08-19 12:11:50 -0400

    Change syntax to is expected, add greater than 0 on amount

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
   describe 'validations' do
-    it { should validate_presence_of(:amount) }
-    it { should validate_numericality_of(:amount) }
+    it { is_expected.to validate_presence_of(:amount) }
+    it { is_expected.to validate_numericality_of(:amount), greater_than: 0 }
   end
 
   describe 'associations' do
