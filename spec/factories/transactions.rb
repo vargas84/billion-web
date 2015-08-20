7f8a854a3f228e5ead2da2d80a761e78f27fe3f4
s12dsow <s12.dsow@gmail.com>
2015-08-19 22:59:52 -0400

    Changed transaction factory

--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :transaction do
     amount { Faker::Number.decimal(2) }
-    association :sender, factory: :TempUser
+    association :sender, factory: :temp_user
     association :recipient, factory: :project
   end
 end
