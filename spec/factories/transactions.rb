130066237b117553646bf9fb7c80d7fb0fc40158
s12dsow <s12.dsow@gmail.com>
2015-08-19 22:57:24 -0400

    Make corrections in transaction factory and spec

--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :transaction do
-    amount Faker::Number.decimal(2)
-    association :sender, factory: :user
+    amount { Faker::Number.decimal(2) }
+    association :sender, factory: :TempUser
     association :recipient, factory: :project
   end
 end
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -3,8 +3,13 @@ require 'rails_helper'
 RSpec.describe Transaction, type: :model do
   describe 'validations' do
     it { is_expected.to validate_numericality_of(:amount).is_greater_than(0.0) }
-    it { is_expected.to validate_inclusion_of(:sender_type).in_array(%w(Project TempUser)).allow_nil }
     it { is_expected.to validate_inclusion_of(:recipient_type).in_array(%w(Project TempUser)) }
+
+    it do
+      is_expected.to validate_inclusion_of(:sender_type)
+        .in_array(%w(Project TempUser))
+        .allow_nil
+    end
   end
 
   describe 'associations' do
