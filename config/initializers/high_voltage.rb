4b63bb1d36627393b021f65582b6d4d5953ec0ce
Alex Vargas <vargas.84@osu.edu>
2015-08-22 12:01:18 -0400

    Add Pages controller to render static pages with different layouts.

--- config/initializers/high_voltage.rb
+++ config/initializers/high_voltage.rb
@@ -1,3 +1,3 @@
 HighVoltage.configure do |config|
-  config.home_page = 'landing'
+  config.routes = false
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
+
   devise_for :users
+
+  root to: 'pages#show', id: 'landing'
+
   post '/subscribe', to: 'subscribe#create', as: 'subscribe'
   resources 'bam_applications', only: :create
-
-  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 end
