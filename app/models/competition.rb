92e7faa7021fbb356d85e758b78b2695aa1f84fb
Alex Vargas <vargas.84buckeye@gmail.com>
2015-09-09 02:00:28 -0400

    Add helper method to get current competition.

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -2,6 +2,11 @@ class Competition < ActiveRecord::Base
   has_many :projects, inverse_of: :competition, dependent: :destroy
   has_many :transactions, inverse_of: :competition, dependent: :destroy
 
-  validates :code_name, :start_date, :end_date, presence: true
+  # TODO: Add validations to ensure only 1 competition per time period
   # TODO: validate that end_date > start_date
+  validates :code_name, :start_date, :end_date, presence: true
+
+  def self.current_competition
+    find_by('start_date < :now and end_date > :now', now: Time.now)
+  end
 end
