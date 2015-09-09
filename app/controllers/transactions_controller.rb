ef485e8523253946deb12c1772a42a62a24636a1
Alex Vargas <vargas.84buckeye@gmail.com>
2015-09-09 02:35:39 -0400

    Add base transacton#create action.

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,18 +1,43 @@
 class TransactionsController < ApplicationController
+  DOLLAR_TO_POINT = 500
+
+  rescue_from ActiveRecord::RecordInvalid, with: :render_error
+
+  before_action :set_temp_user, only: [:create]
+
   def new
     @transaction = Transaction.new(recipient_id: params[:project_id])
   end
 
   def create
-    @transaction = Transaction.new(transaction_params.except(:temp_user))
-    render :new
+    @transaction = Transaction.new transaction_params
+
+    @transaction.points = (@transaction.amount || 0) * DOLLAR_TO_POINT
+    @transaction.sender = @temp_user
+    @transaction.competition = Competition.current_competition
+
+    @transaction.save!
+
+    render :create, status: :created
   end
 
   private
 
   def transaction_params
-    params.require(:transaction)
-      .permit(:amount, :recipient_id, temp_user: [:email])
+    params.require(:transaction).permit(:amount, :recipient_id)
       .merge(recipient_type: 'Project')
+  end
+
+  def set_temp_user
+    transaction = params[:transaction]
+    temp_user = transaction && transaction[:temp_user]
+    email = temp_user && temp_user[:email]
+
+    @temp_user = TempUser.find_or_create_by! email: email
+  end
+
+  def render_error(exception)
+    flash[:error] = exception.record.errors.full_messages
+    render :new, status: :ok
   end
 end
--- spec/controllers/transactions_controller_spec.rb
+++ spec/controllers/transactions_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe TransactionsController, :type => :controller do
+describe TransactionsController, :type => :controller do
 
   describe "GET new" do
     it "returns http success" do
@@ -10,9 +10,64 @@ RSpec.describe TransactionsController, :type => :controller do
   end
 
   describe "GET create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
+    before { create :current_competition }
+
+    let(:project) { create :project }
+    let(:temp_user) { create :temp_user }
+
+    let(:valid_params) do
+      {
+        transaction: {
+          amount: 22.50,
+          recipient_id: project.id,
+          temp_user: {
+            email: temp_user.email
+          }
+        }
+      }
+    end
+
+    context 'successful' do
+      subject(:create_transaction) { post :create, valid_params }
+
+      it { is_expected.to have_http_status(:created) }
+      it { is_expected.to render_template(:create) }
+
+      it 'create a transaction' do
+        expect{ create_transaction }.to change{ Transaction.count }.by(1)
+      end
+    end
+
+    context 'unsuccessful' do
+      shared_examples 'it fails to create a transaction' do
+        it { is_expected.to have_http_status(:ok) }
+        it { is_expected.to render_template(:new) }
+
+        it 'does not create a transaction' do
+          expect{ create_transaction }.to_not change{ Transaction.count }
+        end
+
+        it 'flashes an error' do
+          create_transaction
+          expect(flash[:error]).to be_present
+        end
+      end
+
+      context 'with invalid temp user' do
+        subject(:create_transaction) do
+          post :create, valid_params.tap { |p| p[:transaction].delete(:temp_user) }
+        end
+
+        include_examples 'it fails to create a transaction'
+      end
+
+      context 'with invalid transaction' do
+        subject(:create_transaction) do
+          post :create, valid_params.tap { |p| p[:transaction].delete(:amount) }
+        end
+
+        include_examples 'it fails to create a transaction'
+      end
     end
   end
 
