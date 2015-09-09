3473e226c798953f721433c98d08414205a168dc
Alex Vargas <vargas.84buckeye@gmail.com>
2015-09-09 02:00:58 -0400

    Update competition factories.

--- spec/factories/competitions.rb
+++ spec/factories/competitions.rb
@@ -1,5 +1,5 @@
 FactoryGirl.define do
-  factory :competition do
+  factory :competition, aliases: [:current_competition] do
     code_name { Faker::Lorem.word }
     start_date { Faker::Date.backward(5) }
     end_date { Faker::Date.forward(5) }
@@ -8,6 +8,16 @@ FactoryGirl.define do
       after(:build, :stub) do |competition|
         create_list(:project, 1, competition: competition)
       end
+    end
+
+    factory :previous_competition do
+      start_date { Faker::Date.backward(100) }
+      end_date { Faker::Date.backward(50) }
+    end
+
+    factory :future_competition do
+      start_date { Faker::Date.forward(50) }
+      end_date { Faker::Date.forward(100) }
     end
   end
 end
