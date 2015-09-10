b2aad93baab0dbfe1979da70f1be6862d70e4ef3
Alex Vargas <vargas.84buckeye@gmail.com>
2015-09-09 21:07:41 -0400

    Allow transaction amount to be nil.

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -7,6 +7,6 @@ class Transaction < ActiveRecord::Base
 
   validates :sender_type, inclusion: { in: TYPES, allow_nil: true }
   validates :recipient_type, inclusion: { in: TYPES }
-  validates :amount, numericality: { greater_than: 0.0 }
+  validates :amount, numericality: { greater_than: 0.0 }, allow_nil: true
   validates :points, numericality: { greater_than: 0 }
 end
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
   describe 'validations' do
-    it { is_expected.to validate_numericality_of(:amount).is_greater_than(0.0) }
+    it { is_expected.to validate_numericality_of(:amount).is_greater_than(0.0).allow_nil }
     it { is_expected.to validate_inclusion_of(:recipient_type).in_array(%w(Project TempUser)) }
     it { is_expected.to validate_numericality_of(:points).is_greater_than(0) }
 
